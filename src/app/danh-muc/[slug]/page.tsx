e2cbc7823ed7f207a88a479753e80440434b076c
Ngoc Giang <giang.ngocdao@gmail.com>
2025-06-21 23:15:00 +0700

    update danh-muc

--- src/app/danh-muc/[slug]/page.tsx
+++ src/app/danh-muc/[slug]/page.tsx
@@ -1,4 +1,8 @@
-const DanhMucPage = () => {
+const DanhMucPage = ({ params }: { params: { slug: string } }) => {
+  try {
+    const { slug } = params;
+  } catch (error) {}
+
   return (
     <div className="container">
       <h1 className="text-xl font-black">Danh Mục</h1>
