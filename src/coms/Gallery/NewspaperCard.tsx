fb0f4f7f1c395e281150cd1340bff7ab36313eb8
Hoan Truong <hoantd@olabs.net>
2025-07-15 11:40:26 +0700

    add new paper card hover effect

--- src/coms/Gallery/NewspaperCard.tsx
+++ src/coms/Gallery/NewspaperCard.tsx
@@ -25,16 +25,16 @@ const NewspaperCard = ({ item, catId }: NewspaperCardProps) => {
     <>
       <li key={item?.id} className="" onClick={handleCardClick}>
         <div className="w-full aspect-[3/4] cursor-pointer bg-blue-50 rounded-[12px] flex items-center justify-center">
-          <div className="w-[73%] aspect-[3/4] relative">
+          <div className="w-[73%] aspect-[3/4] relative group">
             <Image
               src={item?.thumbnail}
               alt={item?.name}
               width={227}
               height={317}
-              className="size-full object-cover rounded-[4px] drop-shadow-sm z-20 relative"
+              className="size-full object-cover rounded-[4px] drop-shadow-sm z-20 relative group-hover:scale-105 group-hover:-translate-y-4 transition-all duration-300"
             />
-            <div className="absolute -bottom-2 origin-bottom scale-[95%] rounded-[4px] size-full bg-[#CEDEF3] z-10" />
-            <div className="absolute -bottom-4 origin-bottom scale-[90%] rounded-[4px] size-full bg-[#CEDEF3]/40 z-0" />
+            <div className="absolute bottom-0 translate-y-2 origin-bottom scale-[95%] rounded-[4px] size-full bg-[#CEDEF3] z-10 group-hover:scale-100 group-hover:translate-y-0 transition-all duration-300 delay-75" />
+            <div className="absolute bottom-0 translate-y-4 origin-bottom scale-[90%] rounded-[4px] size-full bg-[#CEDEF3]/40 z-0 group-hover:scale-95 group-hover:translate-y-2 transition-all duration-300 delay-150" />
           </div>
         </div>
         <div className="mt-2.5">
