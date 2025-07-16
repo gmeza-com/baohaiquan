343f87874881b5f7d5a1fbeae31fb01f162a9ccf
Hoan Truong <hoantd@olabs.net>
2025-07-16 10:05:04 +0700

    default use timezone +7

--- src/lib/dayjs.tsx
+++ src/lib/dayjs.tsx
@@ -9,4 +9,17 @@ dayjs.locale(vi);
 
 dayjs.tz.setDefault("Asia/Ho_Chi_Minh");
 
-export default dayjs;
+// Override dayjs() để luôn trả về giờ Việt Nam
+const originalDayjs = dayjs;
+const vietnamDayjs = (...args: any[]) => {
+  if (args.length === 0) {
+    return originalDayjs().tz("Asia/Ho_Chi_Minh");
+  }
+  return originalDayjs(...args);
+};
+
+// Copy all properties from original dayjs
+Object.setPrototypeOf(vietnamDayjs, originalDayjs);
+Object.assign(vietnamDayjs, originalDayjs);
+
+export default vietnamDayjs;
